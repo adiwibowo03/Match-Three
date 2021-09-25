a49bd4aa466401d876ff19ac48efa26cf7db3612
Adi Wibowo <75654137+adiwibowo03@users.noreply.github.com>
2021-09-25 21:17:17 +0700

    Match Three

---
+++ Match Tree/Assets/Scripts/UIScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+public Text highScore;
+public Text currentScore;
+
+private void Update()
+{
+    highScore.text = ScoreManager.Instance.HighScore.ToString();
+    currentScore.text = ScoreManager.Instance.CurrentScore.ToString();
+}
+
+public void Show()
+{
+    gameObject.SetActive(true);
+}
+
+public void Hide()
+{
+    gameObject.SetActive(false);
+}
+}
---
+++ Match Tree/Assets/Scripts/UITime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+public Text time;
+
+private void Update()
+{
+    time.text = GetTimeString(TimeManager.Instance.GetRemainingTime() + 1);
+}
+
+public void Show()
+{
+    gameObject.SetActive(true);
+}
+
+public void Hide()
+{
+    gameObject.SetActive(false);
+}
+
+private string GetTimeString(float timeRemaining)
+{
+    int minute = Mathf.FloorToInt(timeRemaining / 60);
+    int second = Mathf.FloorToInt(timeRemaining % 60);
+
+    return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+}
+}
